678cb9dc60522d3ea130af367f4197597d105f3c
Yao JC <510247045@qq.com>
2020-03-26 18:20:36 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@
  * @Autor: Yao
  * @Date: 2019-11-01 09:33:43
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-03-26 17:33:26
+ * @LastEditTime: 2020-03-26 18:02:30
  */
 
-
+// //
 // let a = require("./c_module").a;
 // import { b } from "./e_module";
 // console.log("a:", a);
@@ -16,15 +16,16 @@
 //   console.log("b:", b);
 // }, 5000);
 
+// //执行加载的文件
 // import './e_action'
 
 
-
+// //本质上，export default就是输出一个叫做default的变量或方法
 // import { default as obj } from "./e_default"
 // console.log(obj)
 
 
-// 使用import()
+// //使用import()动态加载模块
 // import('./e_default').then(module=>{
 //     console.log(module)
 // })
