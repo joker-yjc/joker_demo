26837039a845b3940011223d23782a862ebca097
Yao JC <510247045@qq.com>
2020-05-07 16:27:24 +0800

    no message

--- src/designMode/index.js
+++ src/designMode/index.js
@@ -3,20 +3,31 @@
  * @Author: Yao
  * @Date: 2020-04-24 09:43:47
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-04-24 09:53:53
+ * @LastEditTime: 2020-05-07 15:09:32
  */
-// 观察者模式
-class Subject{
+// 事件触发器
+class EventEmit{
     constructor(){
-        this.observers=[]
+        this.eventObj={};
     }
-
-    // 添加
-    add(observer){
-        this.observers.push(observer)
+    on(ename,cb){
+        if(this.eventObj[ename]){
+            this.eventObj[ename].push(cb)
+        }else{
+            this.eventObj[ename]=[cb]
+        }
     }
-
-    remove(pbserver){
-
+    trigger(ename,...arg){
+        if(this.eventObj[ename]){
+            this.eventObj[ename].forEach(element => {
+                element(...arg)
+            });
+        }
     }
 }
+
+let t=new EventEmit()
+t.on('sayHi',function(name){
+    console.log('hello,'+name)
+})
+t.trigger('sayHi','joke')
