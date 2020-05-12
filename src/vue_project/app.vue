59f7d6102ebf7b67bf1180c2c567652354a85678
Yao JC <510247045@qq.com>
2020-05-12 11:28:20 +0800

    no message

--- src/vue_project/app.vue
+++ src/vue_project/app.vue
@@ -2,17 +2,19 @@
  * @Description: 
  * @Author: Yao
  * @Date: 2020-04-10 17:27:18
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-05-07 18:05:35
+ * @LastEditors: joker_yjc
+ * @LastEditTime: 2020-05-11 11:37:17
  -->
 <template>
-  <div style="text-align: center;">
-    <YjcInput
+  <div style="text-align: center;height:100%">
+    <button @click="handleToggle">toggle</button>
+    <!-- <YjcInput
       v-model="value"
       @change="handleChange"
       placeholder="请输入placeholder"
     />
-    <img src="@/images/joker.jpg" style="height: 100px;" alt="" srcset="" />
+    <img src="@/images/joker.jpg" style="height: 100px;" alt="" srcset="" /> -->
+    <iframe :src="iSrc" frameborder="0" style="height:100%;width:100%"></iframe>
   </div>
 </template>
 <script>
@@ -22,6 +24,7 @@ export default {
   data() {
     return {
       value: '',
+      iSrc:"http://0.0.0.0:8086/statistics/index.html"
     }
   },
   watch: {},
@@ -29,6 +32,11 @@ export default {
     YjcInput,
   },
   methods: {
+    handleToggle(){
+      console.log(this.iSrc)
+      this.iSrc==="http://0.0.0.0:8086/statistics/index.html"?
+      this.iSrc='http://0.0.0.0:8086/statistics/index.html#/commissionWithdrawal':this.iSrc='http://0.0.0.0:8086/statistics/index.html'
+    },
     handleChange(value) {
       console.log(value)
     },
@@ -36,7 +44,9 @@ export default {
 }
 </script>
 <style lang="scss">
-body {
-  color: red;
+html,body {
+  height: 100%;
+  padding: 0;
+  margin:0
 }
 </style>
