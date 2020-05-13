eb356ff2b476499f153774a1d83e00f8801373b1
Yao JC <510247045@qq.com>
2020-05-13 10:29:08 +0800

    no message

--- src/vue_project/components/message.js
+++ src/vue_project/components/message.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-05-12 15:40:04
  * @LastEditors: joker_yjc
- * @LastEditTime: 2020-05-12 16:53:23
+ * @LastEditTime: 2020-05-13 09:25:41
  * @FilePath: /nearby-oa-public/Users/jokeryao/codes/webpack-zip/src/vue_project/components/message.js
  */
 import Message from './Message.vue'
@@ -9,16 +9,18 @@ import Vue from 'vue'
 // 使用基础 Vue 构造器，创建一个“子类”。参数是一个包含组件选项的对象。
 let instance = Vue.extend(Message)
 
-export function message(data){
-  let sa= new instance({data})
+export function message(data) {
+  let sa = new instance({ data })
   sa.$mount()
   document.body.appendChild(sa.$el)
 }
 
-message.error = function (content) {
-  let opt={
-    content,
-    type:"error"
+['error', 'success', 'warn', 'info'].forEach((el) => {
+  message[el] = function (content) {
+    let opt = {
+      content,
+      type: 'error',
+    }
+    message(opt)
   }
-  message(opt)
-}
+})
