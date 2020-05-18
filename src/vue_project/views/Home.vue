2083923f4aaadb7afc487b9f7a95887d6288bda0
Yao JC <510247045@qq.com>
2020-05-18 09:33:17 +0800

    no message

--- src/vue_project/views/Home.vue
+++ src/vue_project/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <button @click="showMessage">show message</button>
-    <YjcInput
-      v-model="value"
-      @change="handleChange"
-      placeholder="请输入placeholder"
-    />
     <img
       :src="require('@/images/joker.jpg')"
       style="height: 100px;"
