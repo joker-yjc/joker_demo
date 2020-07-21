596ab39d8794f08e69761ae7c5744ca54f146fd3
Yao JC <510247045@qq.com>
2020-07-21 09:35:41 +0800

    no message

--- src/vue_project/views/Home.vue
+++ src/vue_project/views/Home.vue
@@ -1,25 +1,33 @@
 <template>
   <div>
-    <button @click="showMessage">show message</button>
-    <img
-      :src="require('@/images/joker.jpg')"
-      style="height: 100px;"
-      alt
-      srcset
-    />
-    webpack-zip/home
+    <button @click="showMessage">show message</button><br>
+    <input type='file' ref='filePicker' @change="fileChange"/>
+    <img :src="src"/><br>
+    <img :src="cSrc"/><br>
+    <div>
+      webpack-zip/home
+    </div>
   </div>
 </template>
 <script>
 import { message } from '../components/Message'
-
+import lrz from 'lrz'
 export default {
   data() {
     return {
       value: '',
+      src:"",
+      cSrc:""
     }
   },
   methods: {
+    fileChange(e){
+      this.src=window.URL.createObjectURL(e.target.files[0])
+      lrz(e.target.files[0]).then(res=>{
+        console.log(res)
+        this.cSrc =window.URL.createObjectURL(res.file)
+      })
+    },
     showMessage() {
       console.log(this.$el)
       message.error('123')
